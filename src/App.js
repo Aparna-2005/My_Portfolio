1a4b7c7da45e09a26ec40da8bdd0b32648aa9e70
Aparna <aparnaravi205@gmail.com>
2026-07-07 16:30:27 +0530

    Bug fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Education from './Components/Education/Education';
 import Project from './Components/Projects/Project';
-import Navbar from './Components/Navbar/Navbar';
 import Certificates from './Components/Certificates/Certificates';
 import Skills from './Components/Skills/Skills';
 import Contact from './Components/Contact/Contact';
@@ -12,7 +11,6 @@ import Contact from './Components/Contact/Contact';
 function App() {
   return (
     <div className="App">
-      {/*  <Navbar/> */}
       <Home/>
       <About/>
       <Education/>
